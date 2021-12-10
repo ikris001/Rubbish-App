89e538c2c4bf054f2a4d617cd10fc811c611c1fe
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2021-12-10 23:36:58 +0000

    designed the layout view for register but have not done the overlay option to switch between both screens yet

---
+++ app/src/main/java/com/example/rubbishapp/RegisterFromLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rubbishapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterFromLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_from_login)
+    }
+}
