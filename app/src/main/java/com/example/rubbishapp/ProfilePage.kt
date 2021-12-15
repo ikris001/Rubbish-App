77eb560f1f0d975bfa2a43789cd5a2402c7b364f
Ben Fawcett (UG) <c0039304@newcastle.ac.uk>
2021-12-15 15:44:00 +0000

    Profile Page UI working

--- app/src/main/java/com/example/rubbishapp/ProfilePage.kt
+++ app/src/main/java/com/example/rubbishapp/ProfilePage.kt
@@ -7,20 +7,9 @@ import android.widget.Button
 
 class ProfilePage : AppCompatActivity() {
 
-    private val editButton = findViewById<Button>(R.id.editBtn)
-    private val user = "Not Current User"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
-
-        editButton.isEnabled = true
-        // hide and disable to edit profile button if the user is viewing another user's profile
-        if (user == "Not Current User"){
-            editButton.setVisibility(View.GONE)
-            editButton.isEnabled = false
-        }
-
-
     }
+
 }
