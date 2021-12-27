4c08b4404c8c55edfcb0d31ec1534692ceb157b3
c0068292@newcastle.ac.uk <you@example.com>
2021-12-27 16:01:49 +0000

    Make Settings Activity have a toolbar.

--- app/src/main/java/com/example/rubbishapp/SettingsActivity.kt
+++ app/src/main/java/com/example/rubbishapp/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.rubbishapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -15,12 +18,36 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+
+        val toolbar: Toolbar = findViewById(R.id.Settings_toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+
+        // show the title defined in the manifest.xml file
+        actionBar?.setDisplayShowTitleEnabled(true)
+
+        // show the back button the the menu bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.settings_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
