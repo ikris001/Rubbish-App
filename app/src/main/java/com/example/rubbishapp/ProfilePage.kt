c66b67d21e0e812b93a7915fdecec4d7e1b5df8d
c0068292@newcastle.ac.uk <you@example.com>
2021-12-27 14:42:57 +0000

    Make Profile Activity have a toolbar.

--- app/src/main/java/com/example/rubbishapp/ProfilePage.kt
+++ app/src/main/java/com/example/rubbishapp/ProfilePage.kt
@@ -2,14 +2,39 @@ package com.example.rubbishapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class ProfilePage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+
+        val toolbar: Toolbar = findViewById(R.id.Profile_toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+
+        // show the title defined in the manifest.xml file
+        actionBar?.setDisplayShowTitleEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.profile_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
