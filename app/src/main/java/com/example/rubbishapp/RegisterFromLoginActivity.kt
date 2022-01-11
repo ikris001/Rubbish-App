db79f17763dc8ec9a13c380fe04b15998f3cd6be
c0068292@newcastle.ac.uk <you@example.com>
2022-01-11 11:05:18 +0000

    add function to create user in database

--- app/src/main/java/com/example/rubbishapp/RegisterFromLoginActivity.kt
+++ app/src/main/java/com/example/rubbishapp/RegisterFromLoginActivity.kt
@@ -5,13 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register_from_login.*
 
 class RegisterFromLoginActivity : AppCompatActivity() {
+    private lateinit var usernameRegister : EditText
+    private lateinit var passwordRegister : EditText
+    private lateinit var confirmPasswordRegister : EditText
+    private lateinit var email : EditText
+    private lateinit var enterButtonRegister : Button
+    private val usersTable = FirebaseDatabase.getInstance().getReference("Users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_from_login)
+
+        usernameRegister = findViewById(R.id.usernameRegister)
+        passwordRegister = findViewById(R.id.passwordRegister)
+        confirmPasswordRegister = findViewById(R.id.confirmPasswordRegister)
+        email = findViewById(R.id.email)
+        enterButtonRegister = findViewById(R.id.enterButtonRegister)
 
         // called loginHereButton id from activity_register_from_login.xml
         // switch between activities register and login
@@ -19,6 +37,14 @@ class RegisterFromLoginActivity : AppCompatActivity() {
         loginHereButton.setOnClickListener {
             val intent1 = Intent(this, LoginActivity::class.java)
             startActivity(intent1)
+        }
+
+        enterButtonRegister.setOnClickListener {
+            if (passwordRegister.text.toString() == confirmPasswordRegister.text.toString()) {
+                val user = User(9, usernameRegister.text.toString(), passwordRegister.text.toString(),
+                email.text.toString(),"","","User",0)
+                createUserInDB(user, usersTable)
+            }
         }
 
 
@@ -47,4 +73,14 @@ class RegisterFromLoginActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private fun createUserInDB(user: User, database: DatabaseReference) {
+        // connects to the DB and creates/overrides the user with the same user.username
+        database.child(user.username).setValue(user).addOnSuccessListener {
+            Toast.makeText(this,"Woo-hoo It's Done :)", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this,"Oh No It's not done :(",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
