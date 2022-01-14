c0c710b3ceeb521e9ad4cf1f36c8d1de258de481
Kristiyan Iliev <kris.iliev001@gmail.com>
2022-01-14 12:29:09 +0000

    Created Report activity and view for users to report areas

---
+++ app/src/main/java/com/example/rubbishapp/ReportActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rubbishapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReportActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
