8db8fff123d857f375fae1ff03ed2c4bd2a3634e
Kristiyan Iliev <kris.iliev001@gmail.com>
2022-01-16 15:42:53 +0000

    Added constructor to Area class

--- app/src/main/java/com/example/rubbishapp/Area.kt
+++ app/src/main/java/com/example/rubbishapp/Area.kt
@@ -1,5 +1,7 @@
 package com.example.rubbishapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.google.android.gms.maps.model.Polygon
 import com.google.android.gms.maps.model.PolygonOptions
 import java.time.LocalDateTime
@@ -8,4 +10,7 @@ data class Area(
     val id:String,
     val shape:PolygonOptions,
     val reportedBy:User?,
-    val dateTime:LocalDateTime)
+    val dateTime:LocalDateTime){
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(): this("", PolygonOptions(), User(), LocalDateTime.now())
+}
