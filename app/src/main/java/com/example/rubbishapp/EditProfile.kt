b474e9b1a8a52eb4b085e7d21604de80837ea1c5
c0068292@newcastle.ac.uk <you@example.com>
2022-01-16 14:51:54 +0000

    fix the buttons in EditProfile Delete Account button now deletes the user from all 3 DBs the Cancel button clears the text in the boxes

--- app/src/main/java/com/example/rubbishapp/EditProfile.kt
+++ app/src/main/java/com/example/rubbishapp/EditProfile.kt
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfile : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    private var deleteAccountFlag = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
@@ -25,6 +32,9 @@ class EditProfile : AppCompatActivity() {
 
         // show the back button the the menu bar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        auth = FirebaseAuth.getInstance()
+        deleteAccountFlag = false
 
         submitBtn.setOnClickListener {
             val newUsername: String = newUsernameField.text.toString()
@@ -41,6 +51,41 @@ class EditProfile : AppCompatActivity() {
             val intent = Intent(this@EditProfile, ProfilePage::class.java)
             startActivity(intent)
         }
+
+        cancelBtn.setOnClickListener {
+            newUsernameField.text!!.clear()
+            newBioField.text!!.clear()
+        }
+
+        deleteBtn.setOnClickListener {
+            if (deleteAccountFlag) {
+                // gets the current user's ID
+                val userID = auth.currentUser?.uid
+
+                // deletes the user's profile pic from the DB
+                FirebaseStorage.getInstance().reference.child("images/$userID.png").delete()
+
+                // deletes the user from the realtime DB
+                FirebaseDatabase.getInstance().getReference("Users/$userID").removeValue()
+
+                // deletes the user from the authentication DB
+                auth.currentUser?.delete()?.addOnSuccessListener {
+                    Toast.makeText(this, "User Deleted", Toast.LENGTH_LONG).show()
+                }
+                // sends the user back to the Login page
+                val intent = Intent(this@EditProfile, LoginActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(
+                    this,
+                    "Are you sure you want to delete you're account?",
+                    Toast.LENGTH_LONG
+                ).show()
+                deleteAccountFlag = true
+            }
+        }
+
     }
 
     private fun changeUsername(NewUsername: String){
