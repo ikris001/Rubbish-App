6153791331e9a2933fa159f370e900bb2bbe12ba
c0068292@newcastle.ac.uk <you@example.com>
2022-01-16 13:31:01 +0000

    copied Ben's code from the deleted editProfile file (this should be in Ben's name)

--- app/src/main/java/com/example/rubbishapp/EditProfile.kt
+++ app/src/main/java/com/example/rubbishapp/EditProfile.kt
@@ -1,10 +1,15 @@
 package com.example.rubbishapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +25,39 @@ class EditProfile : AppCompatActivity() {
 
         // show the back button the the menu bar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        submitBtn.setOnClickListener {
+            val newUsername: String = newUsernameField.text.toString()
+            val newBio: String = newBioField.text.toString()
+
+            if (newUsername.trim().isNotEmpty()){
+                // set new username to user
+                changeUsername(newUsername)
+            }
+            if (newBio.trim().isNotEmpty()){
+                // set new bio to user
+                changeDescription(newBio)
+            }
+            val intent = Intent(this@EditProfile, ProfilePage::class.java)
+            startActivity(intent)
+        }
     }
+
+    private fun changeUsername(NewUsername: String){
+        val database = FirebaseDatabase.getInstance().getReference("Users")
+        val user = Firebase.auth.currentUser
+        val userId = user!!.uid
+        database.child("$userId/username").setValue(NewUsername)
+    }
+
+    private fun changeDescription(NewBio: String) {
+        val database = FirebaseDatabase.getInstance().getReference("Users")
+        val user = Firebase.auth.currentUser
+        val userId = user!!.uid
+        database.child("$userId/bio").setValue(NewBio)
+    }
+
+    fun changeProfilePicture(){}
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
