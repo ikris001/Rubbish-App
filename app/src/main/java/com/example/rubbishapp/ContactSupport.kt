517de374ac8b03b0c2b776a2b1e0858da744b97d
Reem <r.a.a.al-nashrey2@newcastle.ac.uk>
2022-01-20 12:27:31 +0000

    added toolbar in ContactSupport.kt page

--- app/src/main/java/com/example/rubbishapp/ContactSupport.kt
+++ app/src/main/java/com/example/rubbishapp/ContactSupport.kt
@@ -10,13 +10,23 @@ package com.example.rubbishapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.widget.Toolbar
 
 
 class ContactSupport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_support)
+
+        val toolbar: Toolbar = findViewById(R.id.Contact_toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+
+        // show the title defined in the manifest.xml file
+        actionBar?.setDisplayShowTitleEnabled(true)
+
+        // show the back button the the menu bar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
 
     }
